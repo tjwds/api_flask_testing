de7da7516429b628c4ddf387c9b675c9746b462d
tjwds <woodsja@vcu.edu>
2018-04-10 23:29:51 +0000

    serve the saddest grocery list ever

---
+++ run.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+groceries = { "fruit": ["bananas", "pears", {"apples": ["granny smith",
+    "macintosh", "gala"]}],
+            "personal care": ["toothpaste", "chapstick"]}
+
+@app.route('/')
+def hey_world():
+    return jsonify(id=groceries)
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=5000)
